ab567ff8cd047c67532c277d8460db69404a6809
jeff45060 <34991097+jeff45060@users.noreply.github.com>
2022-01-31 12:52:02 -0500

    Completed contact form component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,39 @@ import Layout from "../components/Layout"
 const Contact = () => {
   return (
     <Layout>
-      <h1>Contact</h1>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want To Get In Touch?</h3>
+            <p>
+              Yr lomo tbh venmo typewriter edison bulb keytar cred. Artisan
+              scenester post-ironic migas, jean shorts af echo park.
+            </p>
+            <p>YOLO helvetica 90's plaid tumblr seitan la croix.</p>
+            <p>
+              Cornhole keffiyeh fashion axe, 3 wolf moon cold-pressed edison
+              bulb meh paleo polaroid pop-up asymmetrical chartreuse poutine.
+            </p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">Your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">Your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-">
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button className="btn block">Submit</button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
